15cf8bd878124bd30c35a051b0503492b6be1da4
natrix_dev <discordlenko@gmail.com>
2021-12-10 18:29:01 +0100

    Create ping.js

---
+++ ping.js
@@ -0,0 +1,15 @@
+exports.run = (bot, message) => {
+  message.channel.send(`Pong!`)
+}
+
+exports.help = {
+  name: "ping",
+  description: "Check if the bot is online or not.",
+  usage: "ping"
+}
+
+exports.config = {
+  enabled: true,
+  guildOnly: false,
+  permlevel: 0,
+  aliases: []
